a91a731d48835abf3c7c12fb57cd19521363ef15
AsadGulbahar <asadg1911@gmail.com>
2026-06-01 15:06:54 +0100

    Created frontend UI for bookstore app

---
+++ frontend/script.js
@@ -0,0 +1,145 @@
+const API_URL = 
+    "h p://localhost:3000/books"; 
+ 
+loadBooks(); 
+ 
+function loadBooks() { 
+ 
+    fetch(API_URL) 
+    .then(response => response.json()) 
+    .then(data => { 
+ 
+        const bookList = 
+            document.getElementById("bookList"); 
+ 
+        bookList.innerHTML = ""; 
+ 
+        data.forEach(book => { 
+ 
+            bookList.innerHTML += ` 
+            <div class="book-card"> 
+ 
+                <img src="${book.image}"> 
+ 
+                <h3>${book. tle}</h3> 
+ 
+                <p> 
+                    Author: 
+                    ${book.author} 
+                </p> 
+ 
+                <p> 
+                    Category: 
+                    ${book.category} 
+                </p> 
+ 
+                <p> 
+                    $${book.price} 
+                </p> 
+ 
+                <bu on 
+                class="delete-btn" 
+                onclick="deleteBook(${book.id})"> 
+ 
+                    Delete 
+ 
+                </bu on> 
+ 
+            </div> 
+            `; 
+        }); 
+ 
+    }); 
+} 
+ 
+function addBook() { 
+ 
+    const book = { 
+ 
+        tle: 
+            document.getElementById(" tle").value, 
+ 
+        author: 
+            document.getElementById("author").value, 
+ 
+        price: 
+            document.getElementById("price").value, 
+ 
+        category: 
+            document.getElementById("category").value, 
+ 
+        image: 
+            document.getElementById("image").value 
+    }; 
+ 
+    fetch(API_URL, { 
+ 
+        method: "POST", 
+ 
+        headers: { 
+            "Content-Type": 
+            "applica on/json" 
+        }, 
+ 
+        body: JSON.stringify(book) 
+ 
+    }) 
+    .then(response => response.json()) 
+    .then(() => { 
+ 
+        loadBooks(); 
+ 
+    }); 
+} 
+ 
+function deleteBook(id) { 
+ 
+    fetch( 
+        `${API_URL}/${id}`, 
+        { 
+            method: "DELETE" 
+        } 
+    ) 
+    .then(response => response.json()) 
+    .then(() => { 
+ 
+        loadBooks(); 
+ 
+    }); 
+} 
+ 
+function searchBooks() { 
+ 
+    const keyword = 
+        document.getElementById("search").value; 
+ 
+    fetch( 
+        `h p://localhost:3000/books/search/${keyword}` 
+    ) 
+    .then(response => response.json()) 
+    .then(data => { 
+ 
+        const bookList = 
+            document.getElementById("bookList"); 
+ 
+        bookList.innerHTML = ""; 
+ 
+        data.forEach(book => { 
+ 
+            bookList.innerHTML += ` 
+            <div class="book-card"> 
+ 
+                <img src="${book.image}"> 
+ 
+                <h3>${book. tle}</h3> 
+ 
+                <p>${book.author}</p> 
+ 
+                <p>$${book.price}</p> 
+ 
+            </div> 
+            `; 
+        }); 
+ 
+    }); 
+} 
