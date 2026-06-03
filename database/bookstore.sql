edbf13b31ac0356b7e15d14653904a0cf6c92b01
AsadGulbahar <asadg1911@gmail.com>
2026-06-03 10:35:55 +0100

    updated database schema

--- database/bookstore.sql
+++ database/bookstore.sql
@@ -1,44 +1,69 @@
 #CREATE DATABASE bookstore; 
 USE bookstore;
 
-/*
-CREATE TABLE books 
-( 
-id INT AUTO_INCREMENT PRIMARY KEY, 
-title VARCHAR(255), 
-author VARCHAR(255), 
-price DECIMAL(10,2), 
-category VARCHAR(100), 
-image VARCHAR(500) 
+CREATE TABLE authors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author_id INT,
+    price DECIMAL(10,2),
+    category VARCHAR(100),
+    image VARCHAR(255),
 
-
-INSERT INTO books 
-( title, author, price, category, image) 
-VALUES 
-( 
-'Node.js Guide', 
-'John Smith', 
-29.99, 
-'Programming', 
-'https://picsum.photos/200/300' 
-), 
-( 
-'JavaScript Mastery', 
-'David Brown', 
-34.99, 
-'Programming', 
-'https://picsum.photos/200/301' 
-), 
-( 
-'CSS Design', 
-'Sarah Wilson', 
-19.99, 
-'Web Design', 
-'https://picsum.photos/200/302' 
+    FOREIGN KEY (author_id)
+	REFERENCES authors(id)
 );
-*/
 
+INSERT INTO authors (name)
+VALUES
+('John Smith'),
+('David Brown'),
+('Sarah Wilson');
 
-#SELECT * FROM books;
+INSERT INTO books
+(title, author_id, price, category, image)
+VALUES
+(
+'Node.js Guide',
+1,
+29.99,
+'Programming',
+'nodejs-guide.jpg'
+),
+(
+'JavaScript Mastery',
+2,
+34.99,
+'Programming',
+'javascript-mastery.jpg'
+),
+(
+'CSS Design',
+3,
+19.99,
+'Web Design',
+'css-design.jpg'
+);
+
+INSERT INTO authors (name)
+VALUES
+('John Steinbeck');
+
+INSERT INTO books
+(title, author_id, price, category, image)
+VALUES(
+'Node.js Guide',
+4,
+20.99,
+'Literature',
+'ofmiceandmen.jpg'
+);
+
+UPDATE books
+SET title = 'Of Mice and Men'
+WHERE id = 4;
+SELECT * FROM books;
